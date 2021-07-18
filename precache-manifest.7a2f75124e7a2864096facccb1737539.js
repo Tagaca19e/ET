3537efcc53154ed76b719637dc3d71de6d662c33
James Tagaca <eidmonetagaca@gmail.com>
2021-07-17 21:49:34 -0700

    Updates

---
+++ precache-manifest.7a2f75124e7a2864096facccb1737539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487d24b668e22f37018278b633ed2624",
+    "url": "/ET/index.html"
+  },
+  {
+    "revision": "6adcb18a9c2a282b30b7",
+    "url": "/ET/static/css/main.e6d39443.chunk.css"
+  },
+  {
+    "revision": "3fcb19c2361ca5eb9028",
+    "url": "/ET/static/js/2.2f79dd4e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ET/static/js/2.2f79dd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6adcb18a9c2a282b30b7",
+    "url": "/ET/static/js/main.4b3bdb3a.chunk.js"
+  },
+  {
+    "revision": "15c888493f4b21266f70",
+    "url": "/ET/static/js/runtime-main.ceee3da9.js"
+  }
+]);
